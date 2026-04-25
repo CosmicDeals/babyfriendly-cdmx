6ec6713d1a3a153c512281a91afbc625a234c9f7
CosmicDeals <felicidadquezada@gmail.com>
2026-04-25 01:36:58 -0600

    Update ClickHandler.tsx

--- app/components/ClickHandler.tsx
+++ app/components/ClickHandler.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { LeafletMouseEvent } from 'leaflet'
 import { useMapEvents } from 'react-leaflet'
 
 export default function ClickHandler({ onClic }: { onClic: (lat: number, lng: number) => void }) {
   useMapEvents({
-    click(e) {
+    click(e: LeafletMouseEvent) {
       onClic(e.latlng.lat, e.latlng.lng)
     }
   })
